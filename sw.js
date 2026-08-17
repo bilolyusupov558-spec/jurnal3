7427e20f0f0194b3c3537ecd7bd7245176dbf89c
bahrom yusupov <bilolyusupov558@gmail.com>
2026-08-17 07:09:41 +0500

    first commit

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'jurnal-cache-v1';
+const CORE_ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)).catch(()=>{})
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone)).catch(()=>{});
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
